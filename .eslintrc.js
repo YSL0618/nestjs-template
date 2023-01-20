ebe2177ba446d1dfcacf9c7f8b56fefa5c0e687e
Yuki Sugihara <ken.kiva@gmail.com>
2023-01-20 10:25:30 +0900

    [Fix] Lintをダブルクオーテーション・セミコロン不使用に設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,14 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   root: true,
   env: {
@@ -17,9 +23,9 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    quotes: ["error", "double"],
+    semi: [2, "never"],
+    "no-console": "off",
+    "prettier/prettier": ["error", { semi: false }],
   },
 };
