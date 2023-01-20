7973cfb32f641e6e58b5c8edf30b5a5b66fbdf6d
Yuki Sugihara <ken.kiva@gmail.com>
2023-01-21 00:01:43 +0900

    [Add] Fastily 導入 + Lint 修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,38 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ["@typescript-eslint", "import"],
   extends: [
     "eslint:recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
-    "google",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
   root: true,
   env: {
+    es6: true,
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [".eslintrc.js", "/dist/**/*"],
   rules: {
-    quotes: ["error", "double"],
+    quotes: ["error", "double",{avoidEscape: true}],
     semi: [2, "never"],
     "no-console": "off",
     "prettier/prettier": ["error", { semi: false }],
+
+    camelcase: ["error", { properties: "never" }],
+    // 'capitalized-comments': 'off',
+    "comma-dangle": ["error", "always-multiline"],
+    "comma-spacing": "error",
+    "comma-style": "error",
+    "computed-property-spacing": "error",
   },
-};
+}
